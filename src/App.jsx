cdc1c093ad9d1968668a8bf430cabdfa46965688
sir-sensei <mdabubakar2505@gmail.com>
2025-04-22 09:22:46 +0530

    reset

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex flex-col mt-[10rem] items-center justify-center'>
+      <h1 className='text-[4rem] font-bold'>This site is under progress</h1>
+    </div>
+  )
+}
+
+export default App
