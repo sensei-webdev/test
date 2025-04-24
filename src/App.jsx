2e201ec57b82cd6847e0a47079cb6a4861b6e00e
sir-sensei <mdabubakar2505@gmail.com>
2025-04-24 21:05:55 +0530

    added cname

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import ProfileHeader from "./components/ProfileHeader";
+import ProfileHeader from "./components/ProfileHeader"
 import SkillsSection from "./components/SkillsSection"
+import FeatureCards from './components/FeatureCards'
 
 const App = () => {
   return (
     <main className="flex flex-col mt-[2rem] gap-5 bg-slate-900 text-white">
       <ProfileHeader />
       <SkillsSection />
+      <FeatureCards />
     </main>
   );
 };
---
+++ src/components/FeatureCards.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { FolderIcon, ExternalLinkIcon, GithubIcon } from 'lucide-react'
+
+
+const FeatureCards = () => {
+  return (
+    <main>
+      <h1 className='flex items-center gap-3 text-3xl font-semibold'>
+        <FolderIcon/>
+        Featured Projects
+      </h1>
+      <section>
+        
+      </section>
+    </main>
+  )
+}
+
+export default FeatureCards
